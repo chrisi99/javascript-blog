a59320a65725d55f7b7f00d8fed7109e173a74ca
Krzysztof Podgórski <k4podgorski@gmail.com>
2021-10-24 23:22:36 +0200

    add targetArticle

--- js/script.js
+++ js/script.js
@@ -28,7 +28,9 @@
     const articleSelector = clickedElement.getAttribute('href')
     console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
   
     /* add class 'active' to the correct article */
   }

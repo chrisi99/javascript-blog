7c752f7626151906b28983b884fc1f2e4278de9f
Krzysztof Podgórski <k4podgorski@gmail.com>
2021-10-24 22:17:33 +0200

    Add function remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -14,6 +14,11 @@
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   

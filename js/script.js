98c155270aa7f77a558735f367199d13e1f46b59
Krzysztof Podgórski <k4podgorski@gmail.com>
2021-11-17 18:53:59 +0100

    Add tagClickHandler

--- js/script.js
+++ js/script.js
@@ -84,5 +84,35 @@
       tagsWrapper.innerHTML = html;
     }   
   }
-  generateTags();
+  generateTags()
+
+  const tagClickHandler = function(event){
+    /* prevent default action for this event */
+    event.preventDefault();
+    /* make new constant named "clickedElement" and give it the value of "this" */
+    const clickedElement = this;
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+    const href = clickedElement.getAttribute('href');
+    /* make a new constant "tag" and extract tag from the "href" constant */
+    const tag = href.replace('#tag-', '');
+    /* find all tag links with class active */
+    const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+    /* START LOOP: for each active tag link */
+    for (let tagLink of tagLinks){
+      /* remove class active */
+      tagLink.classList.remove('active');
+    /* END LOOP: for each active tag link */
+    }
+    /* find all tag links with "href" attribute equal to the "href" constant */
+    const targetTags = document.querySelectorAll('a[href="' + href + '"]');
+    /* START LOOP: for each found tag link */
+    for(let targetTag of targetTags){
+      /* add class active */
+      targetTag.classList.add('active');
+    /* END LOOP: for each found tag link */
+    }
+    /* execute function "generateTitleLinks" with article selector as argument */
+    generateTitleLinks('[data-tags~="' + tag + '"]');
+    console.log(generateTitleLinks);
+  }
 }
